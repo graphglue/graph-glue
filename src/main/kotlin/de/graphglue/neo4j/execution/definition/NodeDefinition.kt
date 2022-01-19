071882ad1df521016acf7d6506b8218a45921bf5
nk-coding <niklask.coding@gmail.com>
2022-01-19 22:05:28 +0100

    more documentation

--- src/main/kotlin/de/graphglue/neo4j/execution/definition/NodeDefinition.kt
+++ src/main/kotlin/de/graphglue/neo4j/execution/definition/NodeDefinition.kt
@@ -14,19 +14,52 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 
+/**
+ * Definition of a [Node]
+ * Used to find relationships, get the priamry label, create a Cypher-DSL return expressions
+ *
+ * @property nodeType the class associated with the definition
+ * @property persistentEntity defines Neo4j's view on the node
+ * @param oneRelationshipDefinitions list of all one relationship definitions
+ * @param manyRelationshipDefinitions list of all many relationship definitions
+ */
 class NodeDefinition(
     val nodeType: KClass<out Node>,
     oneRelationshipDefinitions: List<OneRelationshipDefinition>,
     manyRelationshipDefinitions: List<ManyRelationshipDefinition>,
     val persistentEntity: Neo4jPersistentEntity<*>
 ) {
+    /**
+     * Map of all relationship definitions by GraphQL name
+     */
     val relationshipDefinitions =
         (oneRelationshipDefinitions + manyRelationshipDefinitions).associateBy { it.graphQLName }
+
+    /**
+     * map of all relationship definitions by defining property
+     */
     private val relationshipDefinitionsByProperty =
         (oneRelationshipDefinitions + manyRelationshipDefinitions).associateBy { it.property }
+
+    /**
+     * Expression which can be used when creating a query using Cypher-DSL
+     * Fetches all necessary data to map the result to a [Node]
+     */
     val returnExpression: Expression
+
+    /**
+     * Name of the return value in the [returnExpression]
+     */
     val returnNodeName: SymbolicName
+
+    /**
+     * map of all found authorizations from authorization name to [MergedAuthorization]
+     */
     val authorizations: Map<String, MergedAuthorization> = generateAuthorizations()
+
+    /**
+     * The primary label of the [Node]
+     */
     val primaryLabel: String get() = persistentEntity.primaryLabel
 
     init {
@@ -38,6 +71,11 @@ class NodeDefinition(
         returnNodeName = Constants.NAME_OF_TYPED_ROOT_NODE.apply(persistentEntity)
     }
 
+    /**
+     * Creates all authorizations by search for [Authorization] annotations on [nodeType] and all super types
+     *
+     * @return the map of found merged authorizations by authorization name
+     */
     private fun generateAuthorizations(): Map<String, MergedAuthorization> {
         val allAuthorizations = nodeType.springFindRepeatableAnnotations<Authorization>()
         return allAuthorizations.groupBy { it.name }
@@ -56,10 +94,23 @@ class NodeDefinition(
             }
     }
 
+    /**
+     * Gets the Neo4j name of a property
+     *
+     * @param property the property to get the name of
+     * @return the name used by Neo4j
+     */
     fun getNeo4jNameOfProperty(property: KProperty1<*, *>): String {
         return persistentEntity.getGraphProperty(property.name).orElseThrow().propertyName
     }
 
+    /**
+     * Gets the [RelationshipDefinition] by property
+     *
+     * @param property the property to get the relation of
+     * @return the found [RelationshipDefinition]
+     * @throws Exception if property is not used as relation
+     */
     fun getRelationshipDefinitionOfProperty(property: KProperty1<*, *>): RelationshipDefinition {
         return relationshipDefinitionsByProperty[property]!!
     }
--- src/main/kotlin/de/graphglue/neo4j/execution/definition/generateNodeDefinition.kt
+++ src/main/kotlin/de/graphglue/neo4j/execution/definition/generateNodeDefinition.kt
@@ -5,11 +5,19 @@ import de.graphglue.model.NodeProperty
 import de.graphglue.model.NodeRelationship
 import de.graphglue.model.NodeSet
 import org.springframework.data.neo4j.core.mapping.Neo4jMappingContext
+import org.springframework.data.neo4j.core.mapping.Neo4jPersistentEntity
 import kotlin.reflect.KClass
 import kotlin.reflect.KTypeProjection
 import kotlin.reflect.full.*
 import kotlin.reflect.jvm.javaField
 
+/**
+ * Generates a [NodeDefinition] for a specific class
+ *
+ * @param nodeClass the class to generate the definition for
+ * @param mappingContext used to obtain the [Neo4jPersistentEntity]
+ * @return the generated NodeDefinition
+ */
 fun generateNodeDefinition(nodeClass: KClass<out Node>, mappingContext: Neo4jMappingContext): NodeDefinition {
     return NodeDefinition(
         nodeClass,
@@ -19,6 +27,12 @@ fun generateNodeDefinition(nodeClass: KClass<out Node>, mappingContext: Neo4jMap
     )
 }
 
+/**
+ * Generates the [OneRelationshipDefinition]s for a specific [NodeDefinition]
+ *
+ * @param nodeClass the class to generate the relationship definitions for
+ * @return the list of generated relationship definitions
+ */
 private fun generateOneRelationshipDefinitions(nodeClass: KClass<out Node>): List<OneRelationshipDefinition> {
     val properties = nodeClass.memberProperties.filter { it.returnType.isSubtypeOf(Node::class.createType()) }
     return properties.map {
@@ -32,6 +46,12 @@ private fun generateOneRelationshipDefinitions(nodeClass: KClass<out Node>): Lis
     }
 }
 
+/**
+ * Generates the [ManyRelationshipDefinition]s for a specific [NodeDefinition]
+ *
+ * @param nodeClass the class to generate the relationship definitions for
+ * @return the list of generated relationship definitions
+ */
 private fun generateManyRelationshipDefinitions(nodeClass: KClass<out Node>): List<ManyRelationshipDefinition> {
     val nodeListType = NodeSet::class.createType(listOf(KTypeProjection.covariant(Node::class.createType())))
     val properties = nodeClass.memberProperties.filter { it.returnType.isSubtypeOf(nodeListType) }

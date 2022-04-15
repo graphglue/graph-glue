8181a1708c7421984940e8d2cb019e7e4323f581
nk-coding <niklask.coding@gmail.com>
2022-04-15 13:32:15 +0200

    reformat

--- src/main/kotlin/io/github/graphglue/connection/generateConnectionFieldDefinition.kt
+++ src/main/kotlin/io/github/graphglue/connection/generateConnectionFieldDefinition.kt
@@ -24,33 +24,28 @@ import kotlin.reflect.KClass
  * @return the generated field
  */
 fun generateConnectionFieldDefinition(
-    nodeType: KClass<out Node>,
-    name: String,
-    description: String?,
-    transformer: SchemaTransformationContext
+    nodeType: KClass<out Node>, name: String, description: String?, transformer: SchemaTransformationContext
 ): GraphQLFieldDefinition {
     val nodeName = nodeType.getSimpleName()
     val filter = generateFilterDefinition(nodeType, transformer.subFilterGenerator)
     val orders = generateOrders(nodeType, transformer.mappingContext.getPersistentEntity(nodeType.java)!!)
-    return GraphQLFieldDefinition.newFieldDefinition()
-        .name(name).description(description)
-        .argument {
-            it.name("filter").description("Filter for specific items in the connection")
-                .type(filter.toGraphQLType(transformer.inputTypeCache))
-        }.argument {
-            it.name("orderBy").description("Order in which the items are sorted")
-                .type(generateOrderGraphQLType(nodeName, orders, transformer))
-        }.argument {
-            it.name("after").description("Get only items after the cursor").type(Scalars.GraphQLString)
-        }.argument {
-            it.name("before").description("Get only items before the cursor").type(Scalars.GraphQLString)
-        }.argument {
-            it.name("first").description("Get the first n items. Must not be used if before is specified")
-                .type(Scalars.GraphQLInt)
-        }.argument {
-            it.name("last").description("Get the last n items. Must not be used if after is specified")
-                .type(Scalars.GraphQLInt)
-        }.type(GraphQLNonNull(generateConnectionGraphQLType(nodeName, transformer))).build()
+    return GraphQLFieldDefinition.newFieldDefinition().name(name).description(description).argument {
+        it.name("filter").description("Filter for specific items in the connection")
+            .type(filter.toGraphQLType(transformer.inputTypeCache))
+    }.argument {
+        it.name("orderBy").description("Order in which the items are sorted")
+            .type(generateOrderGraphQLType(nodeName, orders, transformer))
+    }.argument {
+        it.name("after").description("Get only items after the cursor").type(Scalars.GraphQLString)
+    }.argument {
+        it.name("before").description("Get only items before the cursor").type(Scalars.GraphQLString)
+    }.argument {
+        it.name("first").description("Get the first n items. Must not be used if before is specified")
+            .type(Scalars.GraphQLInt)
+    }.argument {
+        it.name("last").description("Get the last n items. Must not be used if after is specified")
+            .type(Scalars.GraphQLInt)
+    }.type(GraphQLNonNull(generateConnectionGraphQLType(nodeName, transformer))).build()
 }
 
 /**
@@ -61,37 +56,24 @@ fun generateConnectionFieldDefinition(
  * @return the generated connection [GraphQLOutputType]
  */
 private fun generateConnectionGraphQLType(
-    nodeName: String,
-    transformer: SchemaTransformationContext
+    nodeName: String, transformer: SchemaTransformationContext
 ): GraphQLOutputType {
     val name = "${nodeName}Connection"
     return transformer.outputTypeCache.computeIfAbsent(name, GraphQLTypeReference(name)) {
-        val type =
-            GraphQLObjectType.newObject().name(name).description("The connection type for ${nodeName}.")
-                .field {
-                    it.name("nodes").description("A list of all nodes of the current page.")
-                        .type(GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLTypeReference(nodeName)))))
-                }.field {
-                    it.name("edges").description("A list of all edges of the current page.")
-                        .type(
-                            GraphQLNonNull(
-                                GraphQLList(
-                                    GraphQLNonNull(
-                                        generateEdgeGraphQLType(
-                                            nodeName,
-                                            transformer
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                }.field {
-                    it.name("totalCount").description("Identifies the total count of items in the connection.")
-                        .type(GraphQLNonNull(Scalars.GraphQLInt))
-                }.field {
-                    it.name("pageInfo").description("Information to aid in pagination.")
-                        .type(GraphQLNonNull(GraphQLTypeReference("PageInfo")))
-                }.build()
+        val type = GraphQLObjectType.newObject().name(name).description("The connection type for ${nodeName}.").field {
+            it.name("nodes").description("A list of all nodes of the current page.")
+                .type(GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLTypeReference(nodeName)))))
+        }.field {
+            it.name("edges").description("A list of all edges of the current page.").type(
+                GraphQLNonNull(GraphQLList(GraphQLNonNull(generateEdgeGraphQLType(nodeName, transformer))))
+            )
+        }.field {
+            it.name("totalCount").description("Identifies the total count of items in the connection.")
+                .type(GraphQLNonNull(Scalars.GraphQLInt))
+        }.field {
+            it.name("pageInfo").description("Information to aid in pagination.")
+                .type(GraphQLNonNull(GraphQLTypeReference("PageInfo")))
+        }.build()
 
         transformer.registerFunctionDataFetcher(type, "nodes", Connection::class)
         transformer.registerFunctionDataFetcher(type, "edges", Connection::class)
@@ -116,8 +98,7 @@ private fun generateEdgeGraphQLType(nodeName: String, transformer: SchemaTransfo
             it.name("node").description("The item at the end of the edge.")
                 .type(GraphQLNonNull(GraphQLTypeReference(nodeName)))
         }.field {
-            it.name("cursor").description("A cursor used in pagination.")
-                .type(GraphQLNonNull(Scalars.GraphQLString))
+            it.name("cursor").description("A cursor used in pagination.").type(GraphQLNonNull(Scalars.GraphQLString))
         }.build()
 
         transformer.registerFunctionDataFetcher(type, "node", Edge::class)
@@ -136,21 +117,17 @@ private fun generateEdgeGraphQLType(nodeName: String, transformer: SchemaTransfo
  *         returned
  */
 private fun generateOrderGraphQLType(
-    nodeName: String,
-    orders: Map<String, OrderField<*>>,
-    transformer: SchemaTransformationContext
+    nodeName: String, orders: Map<String, OrderField<*>>, transformer: SchemaTransformationContext
 ): GraphQLInputType {
     val name = "${nodeName}Order"
     return transformer.inputTypeCache.computeIfAbsent(name, GraphQLTypeReference(name)) {
-        GraphQLInputObjectType.newInputObject().name(name)
-            .description("Defines the order of a $nodeName list").field {
-                it.name("field").description("The field to order by, defaults to ID")
-                    .type(generateOrderFieldGraphQLType(nodeName, orders, transformer))
-                    .defaultValueLiteral(EnumValue("ID"))
-            }.field {
-                it.name("direction").description("The direction to order by, defaults to ASC")
-                    .type(GraphQLTypeReference("OrderDirection")).defaultValueLiteral(EnumValue("ASC"))
-            }.build()
+        GraphQLInputObjectType.newInputObject().name(name).description("Defines the order of a $nodeName list").field {
+            it.name("field").description("The field to order by, defaults to ID")
+                .type(generateOrderFieldGraphQLType(nodeName, orders, transformer)).defaultValueLiteral(EnumValue("ID"))
+        }.field {
+            it.name("direction").description("The direction to order by, defaults to ASC")
+                .type(GraphQLTypeReference("OrderDirection")).defaultValueLiteral(EnumValue("ASC"))
+        }.build()
     }
 }
 
@@ -165,8 +142,7 @@ private fun generateOrderFieldGraphQLType(
 ): GraphQLInputType {
     val name = "${nodeName}OrderField"
     return transformer.inputTypeCache.computeIfAbsent(name, GraphQLTypeReference(name)) {
-        val builder =
-            GraphQLEnumType.newEnum().name(name).description("Fields a list of $nodeName can be sorted by")
+        val builder = GraphQLEnumType.newEnum().name(name).description("Fields a list of $nodeName can be sorted by")
         for ((fieldName, fieldValue) in orders.entries) {
             builder.value(fieldName, fieldValue, "Order by ${fieldValue.name}")
         }

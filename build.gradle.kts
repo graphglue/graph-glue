80b66c4aab767912abebc783c25c7e5398206c13
Niklas Krieger <st166864@stud.uni-stuttgart.de>
2022-04-29 14:02:14 +0200

    prepare next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 
 description = "A library to develop annotation-based code-first GraphQL servers using GraphQL Kotlin, Spring Boot and Neo4j"
-version = "1.2.0"
+version = "1.2.1-SNAPSHOT"
 group = "io.github.graphglue"
 
 val graphqlKotlinVersion = "5.3.2"

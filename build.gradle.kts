3c05f9563b942cb3bf390090045546ebea760f09
Niklas Krieger <st166864@stud.uni-stuttgart.de>
2022-04-29 13:56:18 +0200

    release version 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.gradle.DokkaTask
 
 description = "A library to develop annotation-based code-first GraphQL servers using GraphQL Kotlin, Spring Boot and Neo4j"
-version = "1.1.1-SNAPSHOT"
+version = "1.2.0"
 group = "io.github.graphglue"
 
 val graphqlKotlinVersion = "5.3.2"

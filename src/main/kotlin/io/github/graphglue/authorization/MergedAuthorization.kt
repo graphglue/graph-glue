93aa0fc9fcb486b06d6d455542ad56976607f170
nk-coding <niklask.coding@gmail.com>
2022-06-01 11:55:37 +0200

    readded docs to MergedAuthorization

--- src/main/kotlin/io/github/graphglue/authorization/MergedAuthorization.kt
+++ src/main/kotlin/io/github/graphglue/authorization/MergedAuthorization.kt
@@ -1,7 +1,17 @@
 package io.github.graphglue.authorization
 
+import io.github.graphglue.model.Authorization
 import io.github.graphglue.model.Rule
 
+/**
+ * A merged authorization composed of any amount of [Authorization]s
+ *
+ * @param name see [Authorization.name]
+ * @param allow see [Authorization.allow]
+ * @param allowFromRelated see [Authorization.allowFromRelated]
+ * @param disallow see [Authorization.disallow]
+ * @param allowAll see [Authorization.allowAll]
+ */
 data class MergedAuthorization(
     val name: String,
     val allow: Set<Rule>,

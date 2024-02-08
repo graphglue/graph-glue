9765ded42d67e4096fddeca0fb4e4b3e30bab945
nk-coding <niklask.coding@gmail.com>
2024-02-08 15:41:33 +0100

    improve docs

--- graphglue-core/src/main/kotlin/io/github/graphglue/GraphglueCoreConfigurationProperties.kt
+++ graphglue-core/src/main/kotlin/io/github/graphglue/GraphglueCoreConfigurationProperties.kt
@@ -6,9 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
  * Configuration properties for Graphglue core
  *
  * @param maxQueryCost The maximum allowed query complexity
+ * @param useNeo4jPlugin If true, authorization checks use the io.github.graphglue.authorizationPath custom procedure to improve efficiency. This procedure must be provided via a database plugin
  */
 @ConfigurationProperties("graphglue.core")
 class GraphglueCoreConfigurationProperties(
-    val maxQueryCost: Int = 10,
-    val useNeo4jPlugin: Boolean = false
+    val maxQueryCost: Int = 10, val useNeo4jPlugin: Boolean = false
 )
